250ad310b3826d183df9c641aa5bfb2f81d9ebbe
Erik Z <molagg92@gmail.com>
2023-04-01 18:38:51 -0700

    add the final JS code that will correctly meet the requirements for the project. I feel good

--- scripts.js
+++ scripts.js
@@ -5,14 +5,28 @@ function robogerTransciber(userNumberInput){
   let userReturnArray = [];
   for (let i = 0; i <=userNumberInput; i++){
     let NumberToString = i.toString();
-    if (NumberToString.includes('1') && NumberToString.includes('2')){
-      userReturnArray.push("Boop!");
-      
-    }else if (NumberToString.includes('1')){
+
+    if  (NumberToString.includes('2') && NumberToString.includes('1')){
       userReturnArray.push("Beep!");
+    }else if (NumberToString.includes('2') && NumberToString.includes('3')){
+      userReturnArray.push("Won't you be my neighbor?");
     }else if (NumberToString.includes('2')){
       userReturnArray.push("Boop!");
-    }else if (NumberToString.includes("3")){
+    }
+    
+    
+    
+    else if  (NumberToString.includes('1') && NumberToString.includes('2')){
+      userReturnArray.push("Boop!"); 
+    }else if(NumberToString.includes('1') && NumberToString.includes('3')){
+      userReturnArray.push("Won't you be my neighbor?");
+    }else if (NumberToString.includes('1')){
+      userReturnArray.push("Beep!");
+    }
+    
+    
+   
+    else if (NumberToString.includes("3")){
       userReturnArray.push("Won't you be my neighbor?");
     }
     else {
